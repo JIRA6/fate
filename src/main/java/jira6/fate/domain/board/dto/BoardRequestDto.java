7c419e95fbf69f58990fb0772510056076716489
marlboro09 <taebong8989@gmail.com>
2024-07-10 21:45:46 +0900

    🎉 feat : board dto 작성 #1

--- src/main/java/jira6/fate/domain/board/dto/BoardRequestDto.java
+++ src/main/java/jira6/fate/domain/board/dto/BoardRequestDto.java
@@ -1,5 +1,19 @@
 package jira6.fate.domain.board.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BoardRequestDto {
 
+    private String title;
+    private String intro;
+
+    @Builder
+    public BoardRequestDto(String title, String intro) {
+        this.title = title;
+        this.intro = intro;
+    }
 }
--- src/main/java/jira6/fate/domain/board/dto/BoardResponseDto.java
+++ src/main/java/jira6/fate/domain/board/dto/BoardResponseDto.java
@@ -1,5 +1,16 @@
 package jira6.fate.domain.board.dto;
 
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BoardResponseDto {
 
+    private Long id;
+    private Long userid;
+    private LocalDateTime createdAt;
+    private String title;
+    private String intro;
 }
