f2d3797df610bc907a86d00c2b40e29baf281be6
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-07-12 04:16:59 +0900

    🎉 feat: Security 기능 구현
    - Security Config 초기 세팅
    - 인증 필터 초기 세팅
      - 인증 필터 확인을 위해 LoginDto 구현
    - 인가 필터 초기 세팅
    - UserDetails & UserDetailsService 구현체 구현

---
+++ src/main/java/jira6/fate/global/security/UserDetailsImpl.java
@@ -0,0 +1,65 @@
+package jira6.fate.global.security;
+
+import jira6.fate.domain.user.entity.User;
+import jira6.fate.domain.user.entity.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        UserRole userRole = user.getUserRole();
+        String userRoleString = userRole.getUserRole();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(userRoleString);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
