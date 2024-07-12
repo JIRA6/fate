6ef7964e78e78648f5ffb19565eb45ed070dcab4
marlboro09 <taebong8989@gmail.com>
2024-07-12 20:48:52 +0900

    🎉 feat : board entity 생성 #1

--- src/main/java/jira6/fate/domain/board/entity/Board.java
+++ src/main/java/jira6/fate/domain/board/entity/Board.java
@@ -1,16 +1,26 @@
 package jira6.fate.domain.board.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import jira6.fate.domain.user.entity.User;
 import jira6.fate.global.entity.Timestamped;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Columns;
 
 @Entity
+@Table(name = "table_board")
 @Getter
 @NoArgsConstructor
 public class Board extends Timestamped {
@@ -25,9 +35,15 @@ public class Board extends Timestamped {
     @Column(nullable = false)
     private String intro;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Columns> columns = new HashSet<>();
+
     @Builder
     public Board(String title, String intro) {
-
         this.title = title;
         this.intro = intro;
     }
