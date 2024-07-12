163e62e4f540fae63fa4718c1b062b18c2f9df13
Jinny <152242318+co-jinny@users.noreply.github.com>
2024-07-12 19:20:09 +0900

    🎉 feat : Column entity 추가

---
+++ src/main/java/jira6/fate/domain/column/entity/Columns.java
@@ -0,0 +1,51 @@
+package jira6.fate.domain.column.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jira6.fate.domain.board.entity.Board;
+import jira6.fate.global.entity.Timestamped;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "column_table")
+@Getter
+@NoArgsConstructor
+public class Columns extends Timestamped {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String columnName;
+
+  @Column(nullable = false)
+  private Long columnOrder;
+
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "board_id", nullable = false)
+  private Board board;
+
+  @Builder
+  public Columns(String columnName, Long columnOrder, Board board) {
+    this.columnName = columnName;
+    this.columnOrder = columnOrder;
+    this.board = board;
+  }
+
+  public void update(String columnName, Long columnOrder, Board board) {
+    this.columnName = columnName;
+    this.columnOrder = columnOrder;
+    this.board = board;
+  }
+}
