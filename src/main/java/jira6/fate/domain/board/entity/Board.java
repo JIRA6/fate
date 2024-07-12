12d61a66c41815ff99ce537b4fb984984f43f902
Taesoon <165752968+marlboro09@users.noreply.github.com>
2024-07-12 20:51:27 +0900

    feat : Board entity 작성 #1

--- src/main/java/jira6/fate/domain/board/entity/Board.java
+++ src/main/java/jira6/fate/domain/board/entity/Board.java
@@ -1,4 +1,55 @@
 package jira6.fate.domain.board.entity;
 
-public class Board {
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import jira6.fate.domain.user.entity.User;
+import jira6.fate.global.entity.Timestamped;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Columns;
+
+@Entity
+@Table(name = "table_board")
+@Getter
+@NoArgsConstructor
+public class Board extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String intro;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Columns> columns = new HashSet<>();
+
+    @Builder
+    public Board(String title, String intro) {
+        this.title = title;
+        this.intro = intro;
+    }
+
+    public void update(String title, String intro) {
+        this.title = title;
+        this.intro = intro;
+    }
 }
