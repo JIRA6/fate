89b70cf0f2b421b63bc6c5635dc0aa73582239cc
youngju <ysy561356@naver.com>
2024-07-13 16:59:17 +0900

    🩹 fix : JpaRepository 상속 수정

--- src/main/java/jira6/fate/domain/comment/repository/CommentRepository.java
+++ src/main/java/jira6/fate/domain/comment/repository/CommentRepository.java
@@ -2,9 +2,10 @@ package jira6.fate.domain.comment.repository;
 
 import java.util.List;
 import jira6.fate.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByCardId(Long cardId);
 
