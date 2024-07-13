5afdb5d376d2138e54dbd162dfca5137f3585ff4
youngju <ysy561356@naver.com>
2024-07-13 16:45:46 +0900

    🩹 fix : http 메서드 수정

--- src/main/java/jira6/fate/domain/comment/controller/CommentController.java
+++ src/main/java/jira6/fate/domain/comment/controller/CommentController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,7 +42,7 @@ public class CommentController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @PutMapping("/cards/{cardId}/comments")
+    @GetMapping("/cards/{cardId}/comments")
     public ResponseEntity<DataResponse<List<CommentAllResponseDto>>> getAllComment(
         @Min(1) @PathVariable Long cardId
     ) {
