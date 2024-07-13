4ddfd1e59b727ac8cfd33e28b5907014e1031139
youngju <ysy561356@naver.com>
2024-07-13 16:19:06 +0900

    🩹 fix : JpaRepository 상속으로 변경

--- src/main/java/jira6/fate/domain/card/repository/CardRepository.java
+++ src/main/java/jira6/fate/domain/card/repository/CardRepository.java
@@ -2,9 +2,10 @@ package jira6.fate.domain.card.repository;
 
 import java.util.List;
 import jira6.fate.domain.card.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CardRepository extends CrudRepository<Card, Long> {
+public interface CardRepository extends JpaRepository<Card, Long> {
 
     List<Card> findByColumnId(Long columnId);
     List<Card> findByTeamId(Long teamId);
